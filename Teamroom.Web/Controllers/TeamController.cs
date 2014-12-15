b0206b0dadb7f4dd5e933f26dea95fba29c1eff0
Jahmai OSullivan <jaosulli@microsoft.com>
2014-12-15 15:29:36 -0800

    Added team controller

---
+++ Teamroom.Web/Controllers/TeamController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Teamroom.Web.Controllers
+{
+    public class TeamController : Controller
+    {
+        // GET: Team
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
