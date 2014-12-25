daaf8dfe116cd0b49e9cb1736f457938426cd878
Jahmai OSullivan <jaosulli@microsoft.com>
2014-12-25 01:41:26 -0800

    Test class for event repository

---
+++ Teamroom.Tests/HobbyClue.Data/EventRepositoryFacts.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HobbyClue.Data.Repositories;
+using HobbyClue.Tests.Helpers;
+using Xunit;
+
+namespace Teamroom.Tests.HobbyClue.Data
+{
+    public class EventRepositoryFacts
+    {
+        private readonly TestableEventRepository repository = TestableEventRepository.Create();
+
+        [Fact]
+        public void TestTest()
+        {
+            repository.ClassUnderTest.AssignPostToEvent(10, 10);
+        }
+
+        public class TestableEventRepository : Facts<EventRepository>
+        {
+            public static TestableEventRepository Create()
+            {
+                var repo = new TestableEventRepository();
+                return repo;
+            }
+        }
+    }
+}
